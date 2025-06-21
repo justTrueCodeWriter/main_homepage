92844893d07cf198743edf8c08151efe4168d909
justTrueCodeWriter <dmjustcoder8@gmail.com>
2025-06-21 18:32:55 +0300

    add schedule and todo routes

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, json
+from os import environ
+
+from modules import torg
+
+USER = environ["USER"]
+ORG_FILENAME = f"/home/{USER}/org/Orgmode.org"
 
 app = Flask(__name__)
 
@@ -10,3 +16,15 @@ def get_bookmarks() -> dict:
 def index():
     bookmarks = get_bookmarks()
     return render_template('index.html', bookmarks=bookmarks)
+
+@app.route('/get_schedule')
+def get_schedule():
+    return torg.get_schedule(ORG_FILENAME)
+
+@app.route('/get_todo')
+def get_todo():
+    return torg.get_todo(ORG_FILENAME)
+
+@app.route('/get_agenda')
+def get_agenda():
+    return "agenda"
